77d52c78832a1b7eeb2d981a2552430b9a28ac11
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-02-01 12:52:27 -0300

    Starting setting up Junit 5

--- domain/build.gradle.kts
+++ domain/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("java-library")
     id("org.jetbrains.kotlin.jvm")
+    //id("de.mannodermaus.android-junit5")
 }
 
 java {
@@ -12,5 +13,8 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
 
     testImplementation("junit:junit:4.13.2")
+    testImplementation("org.junit.jupiter:junit-jupiter-api:5.7.2")
+    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.7.2")
+    testRuntimeOnly("org.junit.vintage:junit-vintage-engine:5.7.2")
 
 }
--- domain/src/test/java/com/chamwari/tech/domain/usecases/ComputeMonthSummaryUseCaseTest.kt
+++ domain/src/test/java/com/chamwari/tech/domain/usecases/ComputeMonthSummaryUseCaseTest.kt
@@ -1,6 +1,6 @@
 package com.chamwari.tech.domain.usecases
 
-import org.junit.Test
+import org.junit.jupiter.api.Test
 
 
 class ComputeMonthSummaryUseCaseTest {
@@ -16,15 +16,16 @@ class ComputeMonthSummaryUseCaseTest {
 //        }
 //    }
 
-    @Test
+
     fun `test is test`() {
         assert(true)
     }
 
-    @Test
+
     fun `adding two numbers`() {
         assert(4* 4 == 16)
     }
+
 
     @Test
     fun `multiply two numbers`() {
