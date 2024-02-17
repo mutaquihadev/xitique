d1277aab05f2537a2b869cd9af1fef2474647d77
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-02-17 23:30:03 +0100

    Add koin dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -80,6 +80,19 @@ dependencies {
 
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
+
+    // Koin for Android
+    implementation("io.insert-koin:koin-android:3.2.0")
+
+    // Koin Core module
+    implementation("io.insert-koin:koin-core:3.2.0")
+
+    // Koin Test
+    testImplementation("io.insert-koin:koin-test:3.2.0")
+
+    // MockK for mocking in tests
+    testImplementation("io.mockk:mockk:1.12.0")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
