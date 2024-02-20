0f2f08c3d0a22a0b628d08014e10f760d256ef3d
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-02-20 20:39:11 +0100

    Add GetEventsUseCase

--- app/src/main/java/com/chamwari/tech/xitique/domain/usecases/GetEventsUseCase.kt
+++ app/src/main/java/com/chamwari/tech/xitique/domain/usecases/GetEventsUseCase.kt
@@ -1,88 +1,11 @@
 package com.chamwari.tech.xitique.domain.usecases
 
-import com.chamwari.tech.xitique.domain.entities.BalanceSummary
-import com.chamwari.tech.xitique.domain.entities.Event
-import com.chamwari.tech.xitique.domain.entities.EventSummary
-import com.chamwari.tech.xitique.domain.entities.MonthSummary
-import com.chamwari.tech.xitique.domain.entities.MonthlyAggregatedEventSummary
 import com.chamwari.tech.xitique.domain.repositories.EventsRepository
-import com.chamwari.tech.xitique.domain.utils.DateUtils
-import kotlinx.datetime.LocalDateTime
-import kotlin.math.roundToInt
 
-class GetMonthlyAggregatedEventSummaryUseCase(
-    private val userBalance: Int,
-    private val eventCost: Int,
-    private val repository: EventsRepository
-) {
+class GetEventsUseCase(val repository: EventsRepository) {
 
-    fun execute(): MonthlyAggregatedEventSummary {
+    fun execute() {
 
-        val events = repository.getEvents()
-
-        if (events.isEmpty()) {
-            throw IllegalArgumentException("Empty list of events is not computable")
-        }
-
-        val localDateTimesList: List<LocalDateTime> = events.map {
-            val timestamp = it.timestamp
-            DateUtils.timestampToLocalDateTime(timestamp)
-        }
-
-        val relativeBalanceInPercentage = calculateRelativeBalance(events)
-
-        val monthNumber = localDateTimesList.first().month.value
-        val dateOfEvents = localDateTimesList.map { it.dayOfMonth }
-        val monthPrettyName = DateUtils.monthsInPortuguese[monthNumber - 1]
-
-        val monthEventsTotalCost = eventCost * events.size
-        val montBalanceSummary = BalanceSummary(
-            balance = userBalance, relativeBalanceInPercentage = relativeBalanceInPercentage
-        )
-
-        val eventSummaryList = computeEventsSummary(events)
-
-
-        return MonthlyAggregatedEventSummary(
-            monthSummary = MonthSummary(
-                month = monthNumber,
-                monthBalanceSummary = montBalanceSummary,
-                dateOfEvents = dateOfEvents,
-                monthPrettyName = monthPrettyName,
-                totalCost = monthEventsTotalCost
-            ), eventsSummary = eventSummaryList
-        )
     }
 
-    private fun calculateRelativeBalance(events: List<Event>): Int {
-        val balanceRatio = (userBalance * 1f / (eventCost * events.size)) * 100
-        val balance = balanceRatio.roundToInt()
-
-        return when {
-            balance < 0 -> 0
-            balance > 100 -> 100
-            else -> balance
-        }
-    }
-
-    private fun computeEventsSummary(events: List<Event>): List<EventSummary> {
-
-        return events.mapIndexed { index, event ->
-            val balanceAfterEvent = userBalance - (index) * eventCost
-            val ratio = when {
-                 balanceAfterEvent > eventCost -> 100
-                 balanceAfterEvent <= 0 -> 0
-                 else -> ((balanceAfterEvent * 1f / eventCost) * 100).roundToInt()
-            }
-
-            EventSummary(
-                event = event,
-                eventSummary = BalanceSummary(
-                    balanceTitle = "Seu saldo para o evento",
-                    balance = eventCost,
-                    relativeBalanceInPercentage = ratio
-                )
-            )
-        }
-    }
 }
