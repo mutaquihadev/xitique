76abf011d21691802383ec950128bc45a65bef05
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-03-02 21:29:13 +0100

    Split user entity and UserMemberDataResponse classes

---
+++ app/src/main/java/com/chamwari/tech/xitique/data/remote/dto/UserEntity.kt
@@ -0,0 +1,13 @@
+package com.chamwari.tech.xitique.data.remote.dto
+
+import kotlinx.serialization.Serializable
+
+@Serializable
+data class UserEntity(
+    val name: String,
+    val birthDay: Long,
+    val balance: Int,
+    val phoneNumber: String,
+    val isAdmin: Boolean,
+    val isDebug: Boolean = false
+)
--- app/src/main/java/com/chamwari/tech/xitique/data/remote/dto/UserMemberDataResponse.kt
+++ app/src/main/java/com/chamwari/tech/xitique/data/remote/dto/UserMemberDataResponse.kt
@@ -7,12 +7,3 @@ data class UserMemberDataResponse(
     val members: List<UserEntity>
 )
 
-@Serializable
-data class UserEntity(
-    val name: String,
-    val birthDay: Long,
-    val balance: Int,
-    val phoneNumber: String,
-    val isAdmin: Boolean,
-    val isDebug: Boolean = false
-)
