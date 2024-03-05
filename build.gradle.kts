939c8d76c9a3dcace48cba479c3b1f89d662b8c1
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-03-05 23:19:37 +0100

    add ksp

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,4 +5,5 @@ plugins {
     id("org.jetbrains.kotlin.jvm") version "1.9.0" apply false
     id("com.android.library") version "8.2.2" apply false
     id("org.jetbrains.kotlin.plugin.serialization") version "1.6.21"
+    id("com.google.devtools.ksp") version "1.8.10-1.0.9" apply false
 }
