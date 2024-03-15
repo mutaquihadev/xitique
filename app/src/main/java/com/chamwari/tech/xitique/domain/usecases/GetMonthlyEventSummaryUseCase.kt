12fd02cf48c02f5f6d1a6c101e524c05eb497233
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-03-15 23:20:51 +0100

    remove check if events is empty

--- app/src/main/java/com/chamwari/tech/xitique/domain/usecases/GetMonthlyEventSummaryUseCase.kt
+++ app/src/main/java/com/chamwari/tech/xitique/domain/usecases/GetMonthlyEventSummaryUseCase.kt
@@ -1,9 +1,7 @@
 package com.chamwari.tech.xitique.domain.usecases
 
 import com.chamwari.tech.xitique.domain.entities.MonthlyAggregatedEventSummary
-import com.chamwari.tech.xitique.domain.repositories.EventCostRepository
 import com.chamwari.tech.xitique.domain.repositories.EventsRepository
-import com.chamwari.tech.xitique.domain.repositories.UserBalanceRepository
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
@@ -16,12 +14,7 @@ class GetMonthlyAggregatedEventSummaryUseCase(
     fun execute(): Flow<MonthlyAggregatedEventSummary> = flow {
 
         repository.getEvents().collectLatest { events ->
-            if (events.isEmpty()) {
-                throw IllegalArgumentException("Empty list of events is not computable")
-            }
-
             val monthlyAggregatedEventSummary = computeMonthlyEventSummaryUseCase.execute()
-
             emit(monthlyAggregatedEventSummary)
         }
     }
