842cef5bf07ad2770b7572f685ba83c191ff5299
mutaquihadev <olivio.mutaquiha@gmail.com>
2024-03-16 23:44:32 +0100

    remove unused dao operations

--- app/src/main/java/com/chamwari/tech/xitique/data/db/EventDAO.kt
+++ app/src/main/java/com/chamwari/tech/xitique/data/db/EventDAO.kt
@@ -16,20 +16,10 @@ interface EventDAO {
     @Query("SELECT * FROM evententity WHERE id = :id")
     fun getEvent(id: String): Flow<EventEntity>
 
-    @Insert(onConflict = OnConflictStrategy.REPLACE)
-    suspend fun insertEvent(eventEntity: EventEntity)
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertAllEvents(events: List<EventEntity>)
-    @Query("UPDATE evententity SET name = :name, date = :date WHERE id = :id")
-    suspend fun updateEvent(id: String, name: String, date: Long)
 
     @Query("DELETE FROM evententity")
     suspend fun deleteAllEvents()
-
-    @Query("DELETE FROM evententity WHERE id = :id")
-    suspend fun deleteEvent(id: String)
-
-    @Query("DELETE FROM evententity WHERE id IN (:ids)")
-    suspend fun deleteEvents(ids: List<String>)
 }
